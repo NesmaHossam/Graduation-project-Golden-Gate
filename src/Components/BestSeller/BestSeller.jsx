f0cbbd025613364c7791f75e54f2b2b20e48adca
NesmaHossam <nh3643769@gmail.com>
2024-02-19 00:11:11 +0200

    added BestSeller folder

---
+++ src/Components/BestSeller/BestSeller.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./BestSeller.css"
+const BestSeller = () => {
+  return (
+    <div className="best-seller">
+      
+    </div>
+  );
+};
+
+export default BestSeller;
