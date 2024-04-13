308b8c41990cb98e1ceb4e4a4b183ecd42c7972f
Nadakhaledyousif4 <nadakhaledyousif4@gmail.com>
2024-04-13 19:21:05 +0200

    create new file

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -1,18 +1,175 @@
-import React from "react";
-import "./Comments.css"
+import React, { useState } from "react";
+import "./Comments.css";
+import { VscAccount } from "react-icons/vsc";
 
-const Comments = () =>{
-    return(
-        <div className="comments">
+import userprofile from "../Assests/user-profile.png";
 
-{/* <h2>All comments and questions in this course</h2> */}
 
-<div>
 
-</div>
+const Comments = ({ title,username, email, text }) => {
+  const [showCommentInput, setShowCommentInput] = useState(false);
+
+  const toggleCommentInput = () => {
+    setShowCommentInput(!showCommentInput);
+  };
+  
+  return (
+    <div className="comments">
+      <h1>{title}</h1>
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
         </div>
-    )
-}
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+    
 
 
-export default Comments
+
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
+        </div>
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+
+
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
+        </div>
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+
+
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
+        </div>
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+
+
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
+        </div>
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+
+
+
+      <div className="commInfo">
+           <div className="userName">
+           <VscAccount className="VscAccount" />
+           <h2>{username} <br/><h4>{email}</h4></h2>
+           </div>
+
+           <div className="commentBody">
+            <p>{text}</p>
+          </div>
+          
+        <div className="comment-actions">
+        <span onClick={toggleCommentInput}> <i class="bi bi-chat-left-fill"/> 9</span>
+        <span> <i class="bi bi-hand-thumbs-up-fill"/>120</span>
+        <span> <i class="bi bi-hand-thumbs-down-fill"/>5</span>
+        </div>
+       
+        {showCommentInput && (
+        <div className="addcomment">
+            <img src={userprofile} alt="" />
+            <input type="text" placeholder="+ Add comment ........" />
+          </div>
+        )}
+      </div>
+      </div>
+ 
+  );
+};
+
+export default Comments;
--- src/Components/OverView/OverView.jsx
+++ src/Components/OverView/OverView.jsx
@@ -1,14 +1,98 @@
+/*import React from "react";
+import "./OverView.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const OverView = () => {
+  return (
+    <div className="over-view">
+      <div className="About-this-course">
+        <h2>About this course</h2>
+        <p>some important information about this course: what
+        the student need to know and will learn in the course </p>
+      </div>
+      <div className="by-the-num">
+      <p>By the numbers</p>
+      <ul>
+      <li>Skill level: All levels</li>
+      <li>Languages: English</li>
+      <li>Students: 25</li>
+      <li>captions: yes</li>
+      <li>Available on</li>
+      <li>Lectures: 100</li>
+      <li>Total hours: 15 hours</li>
+      </ul>
+      </div> 
+       <div className="Features">
+        <p>Features</p>
+        <ul>
+        <li>
+        Available on .............
+        </li>
+        </ul>
+       </div> 
+       <div className="Description">
+        <p>Description</p>
+        <ul>
+        <li>
+        description in details of the course and what he is doing in the course and every video .
+        description in details of the course and what he is doing in the course and every video .
+        description in details of the course and what he is doing in the course and every video .
+        description in details of the course and what he is doing in the course and every video .
+        description in details of the course and what he is doing in the course and every video .
+        description in details of the course and what he is doing in the course and every video .
+        </li>
+        </ul>
+       </div>
+       <div className="instructor-container">
+       <i class="bi bi-person-circle"></i>
+        <p>Instructor name</p>
+       </div>
+       </div>
+    );
+};
+
+export default OverView;*/
+
 import React from "react";
-import "./OverView.css"
+import "./OverView.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+const OverView = ({ title, coursedescription, skillLevel, languages, students, captions, availableOn, lectures, totalHours, features,description ,instructorName }) => {
+  return (
+    <div className="over-view">
+      <div className="About-this-course">
+        <h2>{title}</h2>
+        <p>{coursedescription}</p>
+      </div>
+      <div className="by-the-num">
+        <p>By the numbers</p>
+        <ul>
+          <li>Skill level: {skillLevel}</li>
+          <li>Languages: {languages}</li>
+          <li>Students: {students}</li>
+          <li>Captions: {captions}</li>
+          <li>Lectures: {lectures}</li>
+          <li>Total hours: {totalHours}</li>
+        </ul>
+      </div>
+      <div className="Features">
+        <p>Features</p>
+        <ul>
+          <li>{features}</li>
+        </ul>
+      </div>
+      <div className="Description">
+        <p>Description</p>
+        <ul>
+          <li>{description}</li>
+        </ul>
+      </div>
+      <div className="instructor-container">
+        <i className="bi bi-person-circle"></i>
+        <p>{instructorName}</p>
+      </div>
+    </div>
+  );
+};
 
-const OverView = () =>{
-
-    return (
-        <div className="over-view">
-
-        </div>
-    )
-}
-
-export default OverView
+export default OverView;
