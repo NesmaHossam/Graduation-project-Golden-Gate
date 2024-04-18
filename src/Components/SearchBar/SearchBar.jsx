a7d543b927c0642342c73497848a9aec64d1d36f
NesmaHossam <nh3643769@gmail.com>
2024-04-18 22:37:14 +0200

    modified searchbar file

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,3 +1,46 @@
+// import React, { useState } from "react";
+// import { FaSearch } from "react-icons/fa";
+// import "./SearchBar.css";
+// import mic_icon from "../Assests/mic_icon.png";
+
+// export const SearchBar = ({ setResults }) => {
+//   const [input, setInput] = useState("");
+
+//   const fetchData = (value) => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         const results = json.filter((user) => {
+//           return (
+//             value &&
+//             user &&
+//             user.name &&
+//             user.name.toLowerCase().includes(value)
+//           );
+//         });
+
+//         setResults(results);
+//       });
+//   };
+
+//   const handleChange = (value) => {
+//     setInput(value);
+//     fetchData(value);
+//   };
+
+//   return (
+//     <div className="input-wrapper">
+//       <FaSearch id="search-icon" />
+//       <input
+//         placeholder="Search"
+//         value={input}
+//         onChange={(e) => handleChange(e.target.value)}
+//       />
+//       <img src={mic_icon} id="mic-icon" alt=""/>
+//     </div>
+//   );
+// };
+
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
@@ -5,6 +48,28 @@ import mic_icon from "../Assests/mic_icon.png";
 
 export const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
+  const [listening, setListening] = useState(false);
+  const recognition = new window.webkitSpeechRecognition();
+
+  const handleSpeechRecognition = () => {
+    if (!listening) {
+      recognition.start();
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript.trim();
+        setInput(transcript);
+        fetchData(transcript);
+        recognition.stop();
+        setListening(false);
+      };
+      recognition.onend = () => {
+        setListening(false);
+      };
+      setListening(true);
+    } else {
+      recognition.stop();
+      setListening(false);
+    }
+  };
 
   const fetchData = (value) => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -36,7 +101,13 @@ export const SearchBar = ({ setResults }) => {
         value={input}
         onChange={(e) => handleChange(e.target.value)}
       />
-      <img src={mic_icon} id="mic-icon" alt=""/>
+      <img
+        src={mic_icon}
+        id="mic-icon"
+        alt="Microphone"
+        onClick={handleSpeechRecognition}
+        style={{ cursor: "pointer" }}
+      />
     </div>
   );
 };
